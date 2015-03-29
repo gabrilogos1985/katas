05f4e52b8348a373eb41ef6f070e6d1017a7bf8b
dSony <Sony@VAIO>
2015-03-29 14:57:16 -0600

    pattern for 4 and six

--- gps/kata/zigzagMatrix/ZigZagMatrix.py
+++ gps/kata/zigzagMatrix/ZigZagMatrix.py
@@ -26,14 +26,38 @@ class ZigzagMatrix(object):
                         yIndex += 1
                         xIndex -= 1
                 counter += 1
-        if (param == 2):
-            startSubMatrix += (";2,2")
+        if param % 2 == 0:
+            xIndex = param
+            yIndex = 2
+            while xIndex > 1:
+                startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+                xIndex -= 1
+                yIndex += 1
+            xIndex = 3
+            yIndex = param
+            while xIndex <= param:
+                startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+                xIndex += 1
+                yIndex -= 1
+        else:
+            xIndex = 2
+            yIndex = param
+            while xIndex <= param:
+                startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+                xIndex += 1
+                yIndex -= 1
         if param == 3:
-            startSubMatrix += (";2,3;3,2;3,3")
+            startSubMatrix += (";3,3")
         if param == 4:
-            startSubMatrix += (";4,2;3,3;2,4;3,4;4,3;4,4")
+            # xIndex = 3
+            # yIndex = 4
+            # while xIndex <= 4:
+            #   startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+            #  xIndex += 1
+            # yIndex -= 1
+            startSubMatrix += (";4,4")
         if param == 5:
-            startSubMatrix += (";2,5;3,4;4,3;5,2;5,3;4,4;3,5;4,5;5,4;5,5")
+            startSubMatrix += (";5,3;4,4;3,5;4,5;5,4;5,5")
         if param == 6:
-            startSubMatrix += ";6,2;5,3;4,4;3,5;2,6;3,6;4,5;5,4;6,3;6,4;5,5;4,6;5,6;6,5;6,6"
+            startSubMatrix += ";6,4;5,5;4,6;5,6;6,5;6,6"
         return [startSubMatrix]
