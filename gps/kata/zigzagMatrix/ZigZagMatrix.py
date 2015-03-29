0c9c1f67d17a5c45112f6831aaaa65538b4bd21b
dSony <Sony@VAIO>
2015-03-29 12:38:38 -0600

    triangulate to find patterns

--- gps/kata/zigzagMatrix/ZigZagMatrix.py
+++ gps/kata/zigzagMatrix/ZigZagMatrix.py
@@ -26,33 +26,14 @@ class ZigzagMatrix(object):
                         yIndex += 1
                         xIndex -= 1
                 counter += 1
-
         if (param == 2):
             startSubMatrix += (";2,2")
-
-        # if param > 2:
-        # # threeSubMatrix = twoSubMatrix + ";3,1;2,2;1,3"
-        # threeSubMatrix = twoSubMatrix
-        # xIndex = 3
-        # yIndex = 1
-        # while xIndex >= 1:
-        # threeSubMatrix += ";" + (str(xIndex) + "," + str(yIndex))
-        # yIndex += 1
-        # xIndex -= 1
-
         if param == 3:
             startSubMatrix += (";2,3;3,2;3,3")
-        # if param > 3:
-        # fourSubMatrix = ""
-        # xIndex = 1
-        # yIndex = 4
-        # while xIndex <= 4:
-        # fourSubMatrix += ";" + (str(xIndex) + "," + str(yIndex))
-        # yIndex -= 1
-        # xIndex += 1
-        # fourSubMatrix = threeSubMatrix + fourSubMatrix
         if param == 4:
             startSubMatrix += (";4,2;3,3;2,4;3,4;4,3;4,4")
         if param == 5:
             startSubMatrix += (";2,5;3,4;4,3;5,2;5,3;4,4;3,5;4,5;5,4;5,5")
+        if param == 6:
+            startSubMatrix += ";6,2;5,3;4,4;3,5;2,6;3,6;4,5;5,4;6,3;6,4;5,5;4,6;5,6;6,5;6,6"
         return [startSubMatrix]
