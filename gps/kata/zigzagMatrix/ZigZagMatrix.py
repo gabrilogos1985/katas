7ff5f8ad0033848bf8f6f07631106309217e58fe
dSony <Sony@VAIO>
2015-03-29 16:09:54 -0600

    And finally Im done i can go home: The algorithm is done :) Well it needs refactor but for now we save the world!!

--- gps/kata/zigzagMatrix/ZigZagMatrix.py
+++ gps/kata/zigzagMatrix/ZigZagMatrix.py
@@ -4,23 +4,22 @@ __author__ = 'Sony'
 class ZigzagMatrix(object):
     @classmethod
     def sort(cls, param):
-        startSubMatrix = "1,1"
+        startSubMatrix = ["1,1"]
         if param > 1:
             counter = 2
             while counter <= param:
                 if counter % 2 == 0:
-                    # twoSubMatrix = ""
                     xIndex = 1
                     yIndex = counter
                     while xIndex <= counter:
-                        startSubMatrix += ";" + (str(xIndex) + "," + str(yIndex))
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
                         yIndex -= 1
                         xIndex += 1
                 else:
                     xIndex = counter
                     yIndex = 1
                     while xIndex >= 1:
-                        startSubMatrix += ";" + (str(xIndex) + "," + str(yIndex))
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
                         yIndex += 1
                         xIndex -= 1
                 counter += 1
@@ -30,25 +29,30 @@ class ZigzagMatrix(object):
                     xIndex = param
                     yIndex = counterSufixMatrix
                     while yIndex <= param:
-                        startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
                         xIndex -= 1
                         yIndex += 1
                 else:
                     xIndex = counterSufixMatrix
                     yIndex = param
                     while xIndex <= param:
-                        startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
                         xIndex += 1
                         yIndex -= 1
         else:
-            xIndex = 2
-            yIndex = param
-            while xIndex <= param:
-                startSubMatrix += ";" + str(xIndex) + "," + str(yIndex)
-                xIndex += 1
-                yIndex -= 1
-        if param == 3:
-            startSubMatrix += (";3,3")
-        if param == 5:
-            startSubMatrix += (";5,3;4,4;3,5;4,5;5,4;5,5")
-        return [startSubMatrix]
+            for counterSufixMatrix in range(2, param + 1):
+                if counterSufixMatrix % 2 == 0:
+                    xIndex = counterSufixMatrix
+                    yIndex = param
+                    while xIndex <= param:
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
+                        xIndex += 1
+                        yIndex -= 1
+                else:
+                    xIndex = param
+                    yIndex = counterSufixMatrix
+                    while yIndex <= param:
+                        startSubMatrix.append(str(xIndex) + "," + str(yIndex))
+                        xIndex -= 1
+                        yIndex += 1
+        return startSubMatrix
--- gps/kata/zigzagMatrix/ZigZagMatrixTest.py
+++ gps/kata/zigzagMatrix/ZigZagMatrixTest.py
@@ -10,25 +10,39 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(["1,1"], ZigzagMatrix.sort(1))
 
     def test_Two(self):
-        self.assertEqual(["1,1;1,2;2,1;2,2"], ZigzagMatrix.sort(2))
+        self.assertEqual("1,1;1,2;2,1;2,2".split(";"), ZigzagMatrix.sort(2))
 
     def test_Three(self):
-        self.assertEqual(["1,1;1,2;2,1;3,1;2,2;1,3;2,3;3,2;3,3"], ZigzagMatrix.sort(3))
+        self.assertEqual("1,1;1,2;2,1;3,1;2,2;1,3;2,3;3,2;3,3".split(";"), ZigzagMatrix.sort(3))
 
     def test_Four(self):
-        self.assertEqual(["1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;4,2;3,3;2,4;3,4;4,3;4,4"], ZigzagMatrix.sort(4))
+        self.assertEqual("1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;4,2;3,3;2,4;3,4;4,3;4,4".split(";"),
+                         ZigzagMatrix.sort(4))
 
     def test_Five(self):
         self.assertEqual(
-            ["1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;5,1;4,2;3,3;2,4;1,5;2,5;3,4;4,3;5,2;5,3;4,4;3,5;4,5;5,4;5,5"],
+            "1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;5,1;4,2;3,3;2,4;1,5;2,5;3,4;4,3;5,2;5,3;4,4;3,5;4,5;5,4;5,5".split(
+                ";"),
             ZigzagMatrix.sort(5))
 
     def test_Six(self):
         self.assertEqual(
-            ["1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;5,1;4,2;3,3;2,4;1,5;1,6;2,5;3,4;4,3;5,2;6,1;6,2;5,3;4,4;3,5;2,6"
-             ";3,6;4,5;5,4;6,3;6,4;5,5;4,6;5,6;6,5;6,6"],
+            "1,1;1,2;2,1;3,1;2,2;1,3;1,4;2,3;3,2;4,1;5,1;4,2;3,3;2,4;1,5;1,6;2,5;3,4;4,3;5,2;6,1;6,2;5,3;4,4;3,5;2,6"
+            ";3,6;4,5;5,4;6,3;6,4;5,5;4,6;5,6;6,5;6,6".split(";"),
             ZigzagMatrix.sort(6))
 
+    def test_printOrderedMatrix(self):
+        size = 11
+        list = ZigzagMatrix.sort(size)
+        for row in range(1, size + 1):
+            for colum in range(1, size + 1):
+                counter = 1
+                for element in list:
+                    if element == str(row) + "," + str(colum):
+                        print('{:^5}'.format(str(counter)), end='')
+                        break
+                    counter += 1;
+            print('')
 
 if __name__ == '__main__':
     unittest.main()
